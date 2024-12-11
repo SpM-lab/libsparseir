4df5859b908e4f3d35eb1728e1b43fd2f3158faa
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-12-11 19:49:32 +0900

    Use inline

--- include/sparseir/_impl.hpp
+++ include/sparseir/_impl.hpp
@@ -6,43 +6,43 @@
 namespace sparseir {
 // General case
 template<typename T>
-T sqrt_impl(const T& x) {
+inline T sqrt_impl(const T& x) {
     return std::sqrt(x);
 }
 
 // Specialization for DDouble
 template<>
-xprec::DDouble sqrt_impl(const xprec::DDouble& x) {
+inline xprec::DDouble sqrt_impl(const xprec::DDouble& x) {
     return xprec::sqrt(x);
 }
 
 template<typename T >
-T cosh_impl(const T& x) {
+inline T cosh_impl(const T& x) {
     return std::cosh(x);
 }
 
 template<>
-xprec::DDouble cosh_impl(const xprec::DDouble& x) {
+inline xprec::DDouble cosh_impl(const xprec::DDouble& x) {
     return xprec::cosh(x);
 }
 
 template<typename T>
-T sinh_impl(const T& x) {
+inline T sinh_impl(const T& x) {
     return std::sinh(x);
 }
 
 template<>
-xprec::DDouble sinh_impl(const xprec::DDouble& x) {
+inline xprec::DDouble sinh_impl(const xprec::DDouble& x) {
     return xprec::sinh(x);
 }
 
 template<typename T>
-T exp_impl(const T& x) {
+inline T exp_impl(const T& x) {
     return std::exp(x);
 }
 
 template<>
-xprec::DDouble exp_impl(const xprec::DDouble& x) {
+inline xprec::DDouble exp_impl(const xprec::DDouble& x) {
     return xprec::exp(x);
 }
 
