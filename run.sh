ced0103478f1b43b26884bdba48047bdf9d0d971
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-04-19 20:43:56 +0900

    Remove run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-#rm -rf ./build && cmake -S . -B ./build -DSPARSEIR_BUILD_TESTING=ON && cmake --build ./build -j && ./build/test/libsparseirtests
-cmake -S . -B ./build -DSPARSEIR_BUILD_TESTING=ON && cmake --build ./build -j && ./build/test/libsparseirtests
