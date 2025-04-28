417d47b2216184d2c8d1d475aebe608ed33ce724
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-04-28 10:15:48 +0900

    WIP

--- test/cinterface_integration.cxx
+++ test/cinterface_integration.cxx
@@ -8,10 +8,10 @@
 #include <random>
 #include <vector>
 
+#include <sparseir/sparseir.hpp>   // C++ interface
 #include <sparseir/sparseir.h>   // C interface
 
 using Catch::Approx;
-using xprec::DDouble;
 
 template <typename S>
 spir_statistics_type get_stat()
@@ -24,26 +24,44 @@ spir_statistics_type get_stat()
 }
 
 template <typename S, typename K>
-void integration_test(double beta, double lambda, double epsilon, int ndim, int dim)
+void integration_test(double beta, double wmax, double epsilon, int ndim, int dim)
 {
     auto stat = get_stat<S>();
+    int32_t status;
 
+    // IR basis
     spir_finite_temp_basis *basis =
         spir_finite_temp_basis_new(stat, beta, wmax, epsilon);
     REQUIRE(basis != nullptr);
+    int32_t basis_size;
+    status = spir_finite_temp_basis_get_size(basis, &basis_size);
+    REQUIRE(status == SPIR_COMPUTATION_SUCCESS);
 
+    // DLR
     spir_dlr *dlr = spir_dlr_new(basis);
     REQUIRE(dlr != nullptr);
-
-    const int npoles = 10;
+    int32_t npoles;
+    status = spir_dlr_get_num_poles(dlr, &npoles);
+    REQUIRE(status == SPIR_COMPUTATION_SUCCESS);
+    REQUIRE(npoles >= 0);
+    REQUIRE(npoles >= basis_size);
     Eigen::VectorXd poles(npoles);
-    Eigen::VectorXd coeffs(npoles);
+    status = spir_dlr_get_poles(dlr, poles.data());
+    REQUIRE(status == SPIR_COMPUTATION_SUCCESS);
+
+    // Generate random DLR coefficients
+    Eigen::VectorXd coeffs(npoles); // Extend to Eigen::Tensor
     std::mt19937 gen(982743);
     std::uniform_real_distribution<> dis(0.0, 1.0);
     for (int i = 0; i < npoles; i++) {
-        poles(i) = wmax * (2.0 * dis(gen) - 1.0);
-        coeffs(i) = 2.0 * dis(gen) - 1.0;
+        // We scale the random coefficients by the square root of the absolute value of the poles
+        // to ensure that all the poles contribute to G(tau) equally.
+        coeffs(i) = (2.0 * dis(gen) - 1.0) * std::sqrt(std::abs(poles(i)));
     }
     REQUIRE(poles.array().abs().maxCoeff() <= wmax);
 
+    // Convert DLR coefficients to IR coefficients
+    Eigen::VectorXd g_IR(basis_size); // Extend to Eigen::Tensor
+    status = spir_dlr_to_IR(dlr, SPIR_ORDER_COLUMN_MAJOR, 1, &basis_size, 0, coeffs.data(), g_IR.data());
+    REQUIRE(status == SPIR_COMPUTATION_SUCCESS);
 }
