9e8626a58d05fc3bc6eb2426438bc529642a21b4
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-05-01 20:38:23 +0900

    Revert changes in build script

--- build_with_tests.sh
+++ build_with_tests.sh
@@ -12,12 +12,13 @@ cd build
 # Configure with tests enabled
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:-$HOME/opt/libsparseir} \
-   -DCMAKE_C_FLAGS="-w -fsanitize=address -g" \
-   -DCMAKE_CXX_FLAGS="-w -fsanitize=address -g" \
-   -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address" \
-  -DSPARSEIR_BUILD_FORTRAN=OFF \
+  -DSPARSEIR_BUILD_FORTRAN=ON \
   -DSPARSEIR_BUILD_TESTING=ON \
   -DBUILD_TESTING=ON
+
+#   -DCMAKE_C_FLAGS="-w -fsanitize=address -g" \
+#   -DCMAKE_CXX_FLAGS="-w -fsanitize=address -g" \
+#   -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address" \
 
 # Build (including tests)
 cmake --build . --config Release -j 4
