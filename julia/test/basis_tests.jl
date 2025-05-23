3438606cd57f4c2bed4d8d3b5e319e3b37f93f52
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-05-24 08:46:12 +0900

    Update tests

--- julia/test/basis_tests.jl
+++ julia/test/basis_tests.jl
@@ -4,28 +4,27 @@
 	β = 2.0
 	ωmax = 5.0
 	ε = 1e-6
+	
+	STATISTICS = [
+		LibSparseIR.SPIR_STATISTICS_BOSONIC,
+		LibSparseIR.SPIR_STATISTICS_FERMIONIC,
+	]
 
-	@testset "Bosonic basis" begin
-		basis = LibSparseIR.spir_bosonic_finite_temp_basis_new(β, ωmax, ε)
+	@testset "Statistics $(stat) " for stat in STATISTICS
+		status = Ref{Int32}(0)
+		k = LibSparseIR.spir_logistic_kernel_new(β * ωmax, status)
+		@test status[] == 0
+		sve = LibSparseIR.spir_sve_result_new(k, ε, status)
+		@test status[] == 0
+		basis = LibSparseIR.spir_basis_new(stat, β, ωmax, k, sve, status)
+		@test status[] == 0
 		@test basis != C_NULL
 		basis_size_ref = Ref{Cint}(0)
-		status = LibSparseIR.spir_bosonic_finite_temp_basis_get_size(basis, basis_size_ref)
+		status = LibSparseIR.spir_basis_get_size(basis, basis_size_ref)
 		@test status == 0
 		basis_size = basis_size_ref[]
 		@test basis_size > 0
-		LibSparseIR.spir_release_bosonic_finite_temp_basis(basis)
-		@test true
-	end
-
-	@testset "Fermionic basis" begin
-		basis = LibSparseIR.spir_fermionic_finite_temp_basis_new(β, ωmax, ε)
-		@test basis != C_NULL
-		basis_size_ref = Ref{Cint}(0)
-		status = LibSparseIR.spir_fermionic_finite_temp_basis_get_size(basis, basis_size_ref)
-		@test status == 0
-		basis_size = basis_size_ref[]
-		@test basis_size > 0
-		LibSparseIR.spir_release_fermionic_finite_temp_basis(basis)
+		LibSparseIR.spir_basis_release(basis)
 		@test true
 	end
 end
