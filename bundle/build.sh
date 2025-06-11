5e85ed86f92b261f2490611be20ee4412955c759
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-06-11 21:53:23 +0900

    WIP

--- bundle/build.sh
+++ bundle/build.sh
@@ -3,24 +3,28 @@
 # Exit on error
 set -e
 
+# Version settings
+EIGEN3_VERSION="3.4.0"
+XPREC_VERSION="0.7.0"
+
 # Create deps directory
 mkdir -p deps
 
 # Download and extract Eigen3
 if [ ! -d "deps/eigen3" ]; then
-    echo "Downloading Eigen3..."
-    wget -q https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -O eigen3.tar.gz
+    echo "Downloading Eigen3 ${EIGEN3_VERSION}..."
+    wget -q https://gitlab.com/libeigen/eigen/-/archive/${EIGEN3_VERSION}/eigen-${EIGEN3_VERSION}.tar.gz -O eigen3.tar.gz
     tar xzf eigen3.tar.gz -C deps
-    mv deps/eigen-3.4.0 deps/eigen3
+    mv deps/eigen-${EIGEN3_VERSION} deps/eigen3
     rm eigen3.tar.gz
 fi
 
 # Download and extract xprec
 if [ ! -d "deps/xprec" ]; then
-    echo "Downloading xprec..."
-    wget -q https://github.com/opencollab/xprec/archive/refs/tags/v1.0.0.tar.gz -O xprec.tar.gz
+    echo "Downloading xprec ${XPREC_VERSION}..."
+    wget -q https://github.com/tuwien-cms/libxprec/archive/refs/tags/v${XPREC_VERSION}.tar.gz -O xprec.tar.gz
     tar xzf xprec.tar.gz -C deps
-    mv deps/xprec-1.0.0 deps/xprec
+    mv deps/libxprec-${XPREC_VERSION} deps/xprec
     rm xprec.tar.gz
 fi
 
