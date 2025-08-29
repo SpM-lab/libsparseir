8fad79c7225d911a236a6f8cb7997720511223fb
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-08-29 14:29:11 +0900

    WIP

--- include/sparseir/contraction.hpp
+++ include/sparseir/contraction.hpp
@@ -10,22 +10,123 @@
 #include <complex>
 #include <tuple>
 
+#ifdef SPARSEIR_USE_BLAS
+extern "C" {
+
+    // double-precision real
+    void cblas_dgemm(
+        const int Order, const int TransA, const int TransB,
+        const int M, const int N, const int K,
+        const double alpha, const double *A, const int lda,
+        const double *B, const int ldb,
+        const double beta, double *C, const int ldc
+    );
+    
+    // double-precision complex
+    void cblas_zgemm(
+        const int Order, const int TransA, const int TransB,
+        const int M, const int N, const int K,
+        const void *alpha, const void *A, const int lda,
+        const void *B, const int ldb,
+        const void *beta, void *C, const int ldc
+    );
+
+    enum CBLAS_TRANSPOSE {
+        CblasNoTrans   = 111,
+        CblasTrans     = 112,
+        CblasConjTrans = 113
+    };
+    
+    enum CBLAS_ORDER {
+        CblasRowMajor = 101,
+        CblasColMajor = 102
+    };
+}
+
+#endif
+
 namespace sparseir {
 
+#ifdef SPARSEIR_USE_BLAS
+// Type-specific CBLAS GEMM implementations
+template<typename U, typename V, typename ResultType>
+void _gemm_blas_impl(const U* A, const V* B, ResultType* C, int M, int N, int K, int lda, int ldb, int ldc);
 
-template <typename T, typename U, typename V>
-Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
-_gemm(const U &A, const V &B)
-{
-    return A * B;
+// Specialization for double * double -> double
+template<>
+inline void _gemm_blas_impl<double, double, double>(const double* A, const double* B, double* C, int M, int N, int K, int lda, int ldb, int ldc) {
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0, A, lda, B, ldb, 0.0, C, ldc);
 }
+
+// Specialization for complex<double> * complex<double> -> complex<double>
+template<>
+inline void _gemm_blas_impl<std::complex<double>, std::complex<double>, std::complex<double>>(const std::complex<double>* A, const std::complex<double>* B, std::complex<double>* C, int M, int N, int K, int lda, int ldb, int ldc) {
+    const std::complex<double> alpha(1.0);
+    const std::complex<double> beta(0.0);
+    cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, &alpha, 
+                 reinterpret_cast<const double*>(A), lda, 
+                 reinterpret_cast<const double*>(B), ldb, 
+                 &beta, reinterpret_cast<double*>(C), ldc);
+}
+
+// Specialization for double * complex<double> -> complex<double>
+template<>
+inline void _gemm_blas_impl<double, std::complex<double>, std::complex<double>>(const double* A, const std::complex<double>* B, std::complex<double>* C, int M, int N, int K, int lda, int ldb, int ldc) {
+    // For mixed types, fall back to Eigen
+    // This could be optimized with custom CBLAS calls if needed
+    // For now, we'll let the caller handle this case
+}
+
+#endif
 
 template <typename U, typename V>
-Eigen::Matrix<decltype(std::declval<typename U::Scalar>() * std::declval<typename V::Scalar>()), Eigen::Dynamic, Eigen::Dynamic>
-_gemm2(const U &A, const V &B)
+Eigen::Matrix<decltype(std::declval<U>() * std::declval<V>()), Eigen::Dynamic, Eigen::Dynamic>
+_gemm(
+    const Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic> &A,
+    const Eigen::Matrix<V, Eigen::Dynamic, Eigen::Dynamic> &B)
 {
-    return A * B;
+    using ResultType = decltype(std::declval<U>() * std::declval<V>());
+    const int M = A.rows();
+    const int N = B.cols();
+    const int K = A.cols();
+    
+    Eigen::Matrix<ResultType, Eigen::Dynamic, Eigen::Dynamic> result(M, N);
+    
+#ifdef SPARSEIR_USE_BLAS
+    // Call appropriate CBLAS function based on input types
+    _gemm_blas_impl<U, V, ResultType>(A.data(), B.data(), result.data(), M, N, K, A.rows(), B.rows(), result.rows());
+#else
+    result = A * B;
+#endif
+    
+    return result;
 }
+
+
+template <typename U, typename V>
+Eigen::Matrix<decltype(std::declval<U>() * std::declval<V>()), Eigen::Dynamic, Eigen::Dynamic>
+_gemm(
+    const Eigen::Map<const Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic>> &A,
+    const Eigen::Map<const Eigen::Matrix<V, Eigen::Dynamic, Eigen::Dynamic>> &B)
+{
+    using ResultType = decltype(std::declval<U>() * std::declval<V>());
+    const int M = A.rows();
+    const int N = B.cols();
+    const int K = A.cols();
+    
+    Eigen::Matrix<ResultType, Eigen::Dynamic, Eigen::Dynamic> result(M, N);
+    
+#ifdef SPARSEIR_USE_BLAS
+    // Call appropriate CBLAS function based on input types
+    _gemm_blas_impl<U, V, ResultType>(A.data(), B.data(), result.data(), M, N, K, A.rows(), B.rows(), result.rows());
+#else
+    result = A * B;
+#endif
+    
+    return result;
+}
+
+
 
 
 template <typename T1, typename T2, int N1, int N2>
@@ -76,7 +177,7 @@ _contract(const Eigen::TensorMap<const Eigen::Tensor<T1, N1>> &tensor1,
     
     // Perform matrix multiplication
     //Eigen::Matrix<ResultType, Eigen::Dynamic, Eigen::Dynamic> result_matrix = matrix1 * matrix2;
-    Eigen::Matrix<ResultType, Eigen::Dynamic, Eigen::Dynamic> result_matrix = _gemm2(matrix1, matrix2);
+    Eigen::Matrix<ResultType, Eigen::Dynamic, Eigen::Dynamic> result_matrix = _gemm(matrix1, matrix2);
     
     // Convert matrix result to tensor and reshape
     Eigen::array<Eigen::Index, N1 + N2 - 2> result_dims;
@@ -225,7 +326,7 @@ _fit_impl_first_dim(const Eigen::JacobiSVD<Eigen::MatrixX<S>> &svd,
     for (int i = 0; i < svd.singularValues().size(); ++i) {
         UHB.row(i) /= ResultType(svd.singularValues()(i));
     }
-    return _gemm2(svd.matrixV(), UHB);
+    return _gemm(svd.matrixV(), UHB);
 }
 
 inline Eigen::MatrixXcd
@@ -256,16 +357,18 @@ _fit_impl_first_dim_split_svd(const Eigen::JacobiSVD<Eigen::MatrixXcd> &svd,
         U_imag = U.block(U_halfsize, 0, U_halfsize, U.cols());
     }
 
-    auto U_imagT = U_imag.transpose();
+    Eigen::MatrixXcd U_imagT = U_imag.transpose();
+    Eigen::MatrixXcd B_real = B.real();
+    Eigen::MatrixXcd B_imag = B.imag();
 
-    Eigen::MatrixXcd UHB = _gemm2(U_realT, B.real());
-    UHB += _gemm2(U_imagT, B.imag());
+    Eigen::MatrixXcd UHB = _gemm(U_realT, B_real);
+    UHB += _gemm(U_imagT, B_imag);
 
     // Apply inverse singular values to the rows of UHB
     for (int i = 0; i < svd.singularValues().size(); ++i) {
         UHB.row(i) /= std::complex<double>(svd.singularValues()(i));
     }
-    return _gemm2(svd.matrixV(), UHB);
+    return _gemm(svd.matrixV(), UHB);
 }
 
 template <typename T, typename S, int N>
